51531f91857315c099eb8929c427b87a7352089d
Gabe Hollombe <gabe.hollombe@foodpanda.com>
2023-03-10 11:42:12 +0800

    first pass at updating affected system

--- affected-systems.mjs
+++ affected-systems.mjs
@@ -37,12 +37,14 @@ export class AffectedSystems extends HTMLElement {
             <h1>Update Affected System</h1>
             <form>
                 <input type="text" name="affected_system" value="">
+                <input type="hidden" name="id" value="">
                 <button type="submit">Update</button>
                 <button type="reset" class="cancel">Cancel</button>
             </form>
       </dialog>
     </section>
     `
+
 
     this.querySelector('form')
       .addEventListener('submit', (e) => {
@@ -58,34 +60,64 @@ export class AffectedSystems extends HTMLElement {
 
     this.subscribedEvents = {
       'NewAffectedSystem': this._new,
+      'UpdateAffectedSystem': this._update,
       'ResolveAffectedSystem': this._resolve,
     }
     for (let item of Object.entries(this.subscribedEvents)) {
       this.eventDispatcher.addListener(item[0], item[1].bind(this))
     }
+
+      this.querySelector('dialog form').addEventListener('submit', e => {
+          e.preventDefault()
+          const updatedName = e.currentTarget.querySelector('input[name="affected_system"]').value
+          const formData = new FormData(e.currentTarget)
+          const id = formData.get('id')
+          this.eventDispatcher.updateAffectedSystem(id, { name: updatedName })
+          this._getDialog().close()
+      })
+
+      this.querySelector('dialog form').addEventListener('reset', e => {
+          this._getDialog().close()
+      })
+  }
+
+  _templatedHtml (affected_system) {
+      const name = affected_system.name
+      return `
+          <span>${affected_system.name}</span> <button>✅</button>
+      `
   }
 
   _new (e) {
     let li = document.createElement('li')
     li.setAttribute('data-id', e.id)
-    li.innerHTML = `<span>${e.details.name}</span> <button>✅</button>`
+      li.innerHTML = this._templatedHtml(e.details)
 
     li.querySelector('button')
       .addEventListener('click', (_) => this.eventDispatcher.resolveAffectedSystem(e.id, { type: 'SUCCESS' }))
 
     li.querySelector('span')
-        .addEventListener('contextmenu', (_) =>  {
-          this.querySelector('dialog').showModal()
+        .addEventListener('contextmenu', (e) =>  {
+          e.preventDefault()
+          let name = li.querySelector('span').innerHTML
+          let dialog = this._getDialog()
+          dialog.querySelector('form input[name="id"]').value = li.dataset.id
+          dialog.querySelector('form input[name="affected_system"]').value = name
+          dialog.showModal()
         })
-
-    this.querySelector('dialog form').addEventListener('submit', e => {
-      e.preventDefault()
-      let data = objectFromForm(new FormData(e.currentTarget))
-    this.eventDispatcher.updateAffectedSystem(e.id, { type: 'SUCCESS' })
 
     this.querySelector('.affected-systems__listing__active ul')
       .appendChild(li)
   }
+
+    _update (e) {
+        let li = this.querySelector(`.affected-systems__listing__active li[data-id="${e.affectedSystemId}"]`)
+
+        // TODO: it feels wrong that _update has to know that the span happens to be where the name lives and this info also
+        //       lives inside the templateHtml for a new LI also. We should come up with a consistent pattern.
+        //       Probably just make the LI content its own webcomponent.
+        li.querySelector('span').innerHTML = e.details.name
+    }
 
   _resolve (e) {
     let li = this.querySelector(`.affected-systems__listing__active li[data-id="${e.affectedSystemId}"]`)
@@ -93,5 +125,9 @@ export class AffectedSystems extends HTMLElement {
     li.querySelector('button').remove()
 
     this.querySelector('.affected-systems__listing__past ul').appendChild(li)
+  }
+
+  _getDialog() {
+      return this.querySelector('dialog')
   }
 }
