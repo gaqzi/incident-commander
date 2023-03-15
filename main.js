fe7b4a5a6e552fc26ab21c07d1a831fca8be144e
Gabe Hollombe <gabe.hollombe@foodpanda.com>
2023-03-15 13:52:38 +0800

    refactor and add handler for websocket close events

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import {ActiveActions} from './actions.mjs'
 import {AffectedSystems} from './affected-systems.mjs'
 import {config} from './config.mjs'
 import {Countdown, CountdownDisplay} from './countdown.mjs'
+import {IncidentSummary} from './incident-summary.mjs'
 import {EventDispatcher} from './event-bus.mjs'
 import {UpdatesSection} from './updates.mjs'
 import {NewIndexedDB} from './storage.mjs'
@@ -14,9 +15,9 @@ import {WebrtcProvider} from 'y-webrtc'
 // components are declared in one place, but it feels weird
 customElements.define('countdown-display', CountdownDisplay)
 customElements.define('countdown-timer', Countdown)
-
 customElements.define('affected-systems', AffectedSystems)
 customElements.define('updates-section', UpdatesSection)
+customElements.define('incident-summary', IncidentSummary)
 
 /******* The stuff from index.html unchanged *******/
 function onElement(el, fn) {
@@ -66,9 +67,30 @@ function StoreEvents(events, storage) {
     // Multi-user collab events with YJS
     const ydoc = new Y.Doc()
     const signaling = [process.env.WEBRTC_SIGNALING_SERVER] // injected via parcel. see README for more info
-    const room = "InCom-HelloWorld" // TODO: use incidentID here?, from query string?
-    const password = "funtimes123" //  TODO: take from query string?
+
+    // If we don't have a room and password, create them and refresh window so they're on the query string
+    params = new URLSearchParams(window.location.search)
+    if (!params.get('room')) {
+        params.set('room', new Date().valueOf())
+    }
+    if (!params.get('password')) {
+        params.set('password', parseInt(Math.random() * 10e5, 10))
+    }
+    if ((new URLSearchParams(window.location.search)).toString() !== params.toString()) {
+        window.location.search = params.toString()
+    }
+
+    const room = params.get('room')
+    const password = params.get('password')
     const provider = new WebrtcProvider(room, ydoc, {signaling: signaling, password: password})
+
+    // If we can't connect to the signaling server, show an error dialog for now I guess?
+    const sc = provider.signalingConns[0]
+    const ws = sc.ws
+    // Note: websocket errors are super useless, see https://stackoverflow.com/questions/18803971/websocket-onerror-how-to-read-error-description
+    ws.addEventListener('close', (event) => {
+        window.alert(`Failed to get/stay connected to peers.\nMaybe the signaling server (${signaling}) is down?`)
+    })
 
     let events = new EventDispatcher(null, null, ydoc)
     StoreEvents(events, db)
@@ -136,7 +158,7 @@ function StoreEvents(events, storage) {
         onElement(findParentElementWithClass(e.target, 'incident-summary'), el => el.classList.toggle('closed'))
     })
 
-// Actions
+    // Actions
     document.querySelector('.actions__add form').addEventListener('submit', e => {
         e.preventDefault()
 
@@ -147,131 +169,6 @@ function StoreEvents(events, storage) {
 
         e.currentTarget.reset()
     })
-
-
-    class IncidentSummary extends HTMLElement {
-        /**
-         *
-         * @param {string} id
-         * @param {EventDispatcher} eventDispatcher
-         */
-        constructor(id, eventDispatcher) {
-            super()
-            this.id = id
-            this.eventDispatcher = eventDispatcher
-
-            this.el = document.getElementById('incident-summary').content.cloneNode(true)
-            this.appendChild(this.el)
-
-            for (let attr of IncidentSummary.observedAttributes) {
-                this.querySelector(`.${attr}`).addEventListener('contextmenu', e => {
-                    e.preventDefault()
-
-                    let currentVal = this.getAttribute(attr)
-                    let newVal = null
-                    if (attr === 'status') { // XXX: blergh, but making nicer later!
-                        currentVal = config.statuses.map(s => s === currentVal ? `[${s}]` : s).join(' / ')
-                        let first = true
-                        do {
-                            newVal = prompt('New value' + first ? '' : ', need to pick a valid status', currentVal)
-                            first = false
-                            if (newVal === null) continue
-                            newVal = newVal.trim()
-                        } while (!config.statuses.includes(newVal))
-                    } else {
-                        newVal = prompt('New value', currentVal)
-                        if (newVal === null) return
-                    }
-
-                    let data = {}
-                    data[attr] = newVal
-                    this.eventDispatcher.updateIncident(this.id, data)
-                })
-            }
-
-            this.eventDispatcher.addListener('UpdateIncident', e => {
-                for (let kv of Object.entries(e.details)) {
-                    this.setAttribute(kv[0], kv[1])
-                }
-            })
-
-            this.querySelector('dialog form').addEventListener('submit', e => {
-                e.preventDefault()
-                let data = objectFromForm(new FormData(e.currentTarget))
-                if (data.id === '') {
-                    delete data.id
-                    this.eventDispatcher.addResourceLink(data)
-                } else {
-                    let id = data.id
-                    delete data.id
-                    this.eventDispatcher.updateResourceLink(id, data)
-                }
-
-                e.currentTarget.reset()
-                e.currentTarget.querySelector('input[name="id"]').value = ''
-                this.querySelector('dialog').close()
-            })
-
-            this.querySelector('.add-link').addEventListener('click', e => {
-                e.preventDefault()
-                let dialog = document.querySelector('dialog')
-                dialog.showModal()
-            })
-            this.querySelector('dialog button[type="reset"]')
-                .addEventListener('click', e => {
-                    this.querySelector('dialog input[name="id"]').value = ''
-                    this.querySelector('dialog').close(null)
-                })
-
-            for (let eventName of ['AddResourceLink', 'UpdateResourceLink']) {
-                this.eventDispatcher.addListener(eventName, this._handleLinkEvents.bind(this))
-            }
-        }
-
-        _handleLinkEvents(e) {
-            let el = null
-            switch (e.name) {
-                case 'AddResourceLink':
-                    let list = this.querySelector('.incident-summary__links__list')
-                    el = document.createElement('li')
-                    el.innerHTML = `<a href="${e.details.url}" target="_blank" class="external" data-id="${e.id}">${e.details.description}</a>`
-                    el.addEventListener('contextmenu', this._showUpdateLinkDialog.bind(this))
-                    list.appendChild(el)
-                    break
-                case 'UpdateResourceLink':
-                    el = this.querySelector(`.incident-summary__links__list a[data-id="${e.resourceLinkId}"]`)
-                    if (el === null) throw `Unable to find link to update: ${e.id}, ${JSON.stringify(e.details)}`
-
-                    el.setAttribute('href', e.details.url)
-                    el.innerHTML = e.details.description
-                    break
-                default:
-                    throw `Unable to handle link event: ${e.name}`
-            }
-        }
-
-        _showUpdateLinkDialog(e) {
-            e.preventDefault()
-
-            let dialog = this.querySelector('dialog')
-            dialog.querySelector('input[name="description"]').value = e.target.innerHTML
-            dialog.querySelector('input[name="url"]').value = e.target.getAttribute('href')
-            dialog.querySelector('input[name="id"]').value = e.target.dataset.id
-            dialog.showModal()
-        }
-
-        static get observedAttributes() {
-            return ['what', 'when', 'where', 'impact', 'status']
-        }
-
-        attributeChangedCallback(name, oldValue, newValue) {
-            if (oldValue === newValue) return
-
-            this.querySelector(`.message .${name}`).innerText = newValue
-        }
-    }
-
-    customElements.define('incident-summary', IncidentSummary)
 
     let createIncidentHandler = e => {
         let is = new IncidentSummary(e.id, events)
