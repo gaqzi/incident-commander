4abe004d48ad36cd4fa105b96c8dbacfe600657e
Gabe Hollombe <gabe.hollombe@foodpanda.com>
2023-05-10 14:11:23 +0800

    socket relay server rename

--- socket_relay_server/server.js
+++ socket_relay_server/server.js
@@ -30,6 +30,6 @@ server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, handleAuth)
 })
 
-server.listen(port, host, () => {
-  console.log(`running at '${host}' on port ${port}`)
+server.listen(port, () => {
+  console.log(`running on port ${port}`)
 })
