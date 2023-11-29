d1221959bbf106cdc9a4c9dc9a4d79c3632e3eaa
Gabe Hollombe <gabe.hollombe@foodpanda.com>
2023-11-29 09:05:32 +0800

    update home page copy

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,11 @@ export default function Home() {
         <ul className="list-disc list-inside mt-1">
           <li>Keeping track of all the current issues affecting your systems</li>
           <li>Managing a list of actions per issue</li>
+          <li>Keeping track of a timeline of notes for each action</li>
           <li>Tracking who is working on what action(s)</li>
           <li>Reminding you when to ask for updates from people</li>
           <li>Making it easy to copy business & tech summaries to your clipboard so you can keep others updated</li>
+          <li>Providing a shared notes area for free-form (also multiplayer-compatible) organization and collaboration</li>
         </ul>
 
         <h1 className="mt-4">Getting Started</h1>
@@ -38,10 +40,10 @@ export default function Home() {
 
         <h1 className="mt-4">What does it look like and how does it work?</h1>
 
-        <p className="mt-2">Here&apos;s a short demo video:</p>
+        <h3 className="mt-2">Here&apos;s a short demo video. (Note: the video is a little out of date due to new features and further UI polish)</h3>
         <iframe className="border-0" width="560" height="315" src="https://www.youtube.com/embed/1F2GK-zinls?si=v5sz4lFqz6izkeIS" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
 
-        <p className="mt-2">And here&apos;s a sample screenshot:</p>
+        <h3 className="mt-2">And here&apos;s a sample screenshot:</h3>
         <p className="mt-2">
           <Image priority alt="screenshot" src={screenshot} className="border-t-4 border-l-4 border-r-8 border-b-8 border-black" />
         </p>
